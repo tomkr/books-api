480a81c576e8ac5b6ad8a45674cd683a3321fbe6
Tom Kruijsen <tom@tomkruijsen.nl>
2017-01-07 18:23:27 +0100

    Move step definitions
    move the more general steps out of the books step definitions.

---
+++ features/step_definitions/web.rb
@@ -0,0 +1,36 @@
+# Collection of step definitions to do with handling general requests and
+# responses.
+
+Given(/^a user requests "([^"]*)"$/) do |path|
+  get path
+end
+
+Given(/^a user posts '([^']*)' to "([^"]*)"$/) do |body, path|
+  post path, body, 'CONTENT_TYPE' => 'application/json'
+end
+
+Then(/^the response should have "([^"]*)" "([^"]*)"$/) do |key, value|
+  expect(parsed_response[key]).to eq(value)
+end
+
+Then(/^the response should have a "([^"]*)" link to "([^"]*)"$/) do |key, path|
+  expect(parsed_response['_links'][key]['href']).to eq(path)
+end
+
+Then(/^the status should be (\d+)$/) do |code|
+  expect(last_response.status).to eq(code.to_i)
+end
+
+Then(/^the response has embedded "([^"]*)" with$/) do |list, table|
+  list = parsed_response['_embedded'][list]
+  hashes = table.hashes
+  keys = hashes.first.keys
+  keys.each do |k|
+    expect(hashes.map { |v| v.fetch(k) })
+      .to match_array(list.map { |v| v.fetch(k) })
+  end
+end
+
+def parsed_response
+  JSON.parse(last_response.body)
+end
