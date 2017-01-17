cee87fa593246ad036c5067ece5bb3bf6043bc50
Tom Kruijsen <tom@tomkruijsen.nl>
2017-01-17 21:00:04 +0100

    Update seed data
    Add slugs for all models.

--- db/seed.rb
+++ db/seed.rb
@@ -4,24 +4,38 @@ require './app/models/author'
 require './app/models/book'
 require './app/models/serie'
 
+# Help creating slugs
+def sluggify_params(field_to_sluggify, params)
+  params.merge(slug: Sluggify.sluggify(params[field_to_sluggify]))
+end
+
 # Seed data to get the app started.
 Serie.destroy_all
 Book.destroy_all
 Author.destroy_all
 
-sanderson = Author.create(name: 'Brandon Sanderson')
-martin = Author.create(name: 'George R. R. Martin')
+sanderson = Author.create(sluggify_params(:name, name: 'Brandon Sanderson'))
+martin = Author.create(sluggify_params(:name, name: 'George R. R. Martin'))
 
-mistborn = Serie.create(title: 'Mistborn')
-ice_and_fire = Serie.create(title: 'A Song of Ice and Fire')
+mistborn = Serie.create(sluggify_params(:title, title: 'Mistborn'))
+ice_and_fire = Serie.create(sluggify_params(:title,
+                                            title: 'A Song of Ice and Fire'))
 
-Book.create(title: 'Elantris', author: sanderson)
-Book.create(title: 'The Final Empire', author: sanderson, serie: mistborn)
-Book.create(title: 'The Well of Ascension', author: sanderson, serie: mistborn)
-Book.create(title: 'The Hero of Ages', author: sanderson, serie: mistborn)
+Book.create(sluggify_params(:title, title: 'Elantris', author: sanderson))
+Book.create(sluggify_params(:title, title: 'The Final Empire',
+                                    author: sanderson, serie: mistborn))
+Book.create(sluggify_params(:title, title: 'The Well of Ascension',
+                                    author: sanderson, serie: mistborn))
+Book.create(sluggify_params(:title, title: 'The Hero of Ages',
+                                    author: sanderson, serie: mistborn))
 
-Book.create(title: 'A Game of Thrones', author: martin, serie: ice_and_fire)
-Book.create(title: 'A Clash of Kings', author: martin, serie: ice_and_fire)
-Book.create(title: 'A Storm of Swords', author: martin, serie: ice_and_fire)
-Book.create(title: 'A Feast for Crows', author: martin, serie: ice_and_fire)
-Book.create(title: 'A Dance with Dragons', author: martin, serie: ice_and_fire)
+Book.create(sluggify_params(:title, title: 'A Game of Thrones',
+                                    author: martin, serie: ice_and_fire))
+Book.create(sluggify_params(:title, title: 'A Clash of Kings',
+                                    author: martin, serie: ice_and_fire))
+Book.create(sluggify_params(:title, title: 'A Storm of Swords',
+                                    author: martin, serie: ice_and_fire))
+Book.create(sluggify_params(:title, title: 'A Feast for Crows',
+                                    author: martin, serie: ice_and_fire))
+Book.create(sluggify_params(:title, title: 'A Dance with Dragons',
+                                    author: martin, serie: ice_and_fire))
