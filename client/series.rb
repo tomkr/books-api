7b1b635c65ad5282c62b0e226619d6e73f72fe82
Tom Kruijsen <tom@tomkruijsen.nl>
2017-01-24 07:54:57 +0100

    Add CLI commands for series

--- client/series.rb
+++ client/series.rb
@@ -9,7 +9,33 @@ class Series < Thor
     puts client.series.get.map(&:title).join("\n")
   end
 
+  desc 'add TITLE', 'Add a serie'
+  def add(title)
+    serie = client.series.post(title: title).body
+    puts "#{serie['title']} has been added"
+  end
+
+  desc 'edit TITLE NEW_TITLE', 'Edit an existing serie'
+  def edit(title, new_title)
+    serie = find_by_title(title)
+    return puts "#{title} not found" if serie.nil?
+    new_serie = serie.put(title: new_title).body
+    puts "#{new_serie['title']} has been updated"
+  end
+
+  desc 'delete TITLE', 'Remove an existing serie'
+  def delete(title)
+    serie = find_by_title(title)
+    return puts "#{title} not found" if serie.nil?
+    serie.delete
+    puts "#{title} has been removed"
+  end
+
   private
+
+  def find_by_title(title)
+    client.series.get.select { |s| s.title == title }.first
+  end
 
   def client
     @client ||= APIClient.new
